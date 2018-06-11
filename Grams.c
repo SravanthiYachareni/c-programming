e9273afc506c8ffeedc2b972d411eb2acfc21329
Sravanthi Yachareni <sravanthiyachareni@gmail.com>
2018-06-11 17:24:18 +0530

    Grams.c added

---
+++ Grams.c
@@ -0,0 +1,16 @@
+/* Program to find to convert to kilorams to grams */
+/* Author:Sravanthi_YAchareni*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float kg, gr;
+printf("Enter Kilogram");
+scanf("%f",&kg);
+gr=1000*kg;
+printf("%f Grams",gr);
+}
+
+
+  
+
