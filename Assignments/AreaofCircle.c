53079e307b98585700ae1f85610d070702671577
Sravanthi Yachareni <sravanthiyachareni@gmail.com>
2018-06-14 13:15:07 +0530

    modified

--- Assignments/AreaofCircle.c
+++ Assignments/AreaofCircle.c
@@ -4,11 +4,11 @@
 #include<conio.h>
 void main()
 {	
-float r,A,C;
+float radius,Area,Circumference;
 printf("Enter radius of a circle");
-scanf("%f",&r);
-A=3.147*r*r;                    /* Area of a circle=3.147*r*r */
-C=2*3.147*r;                    /* Circumfernce of a circle =2*3.147*r */
-printf("Area of a circle is:%0.2f\n",A)
-printf("Circumference of a circle is:%0.2f",C);
+scanf("%f",&radius);
+Area=3.147*radius*radius;                    /* Area of a circle=3.147*r*r */
+Circumference=2*3.147*radius;                    /* Circumfernce of a circle =2*3.147*r */
+printf("Area of a circle is:%0.2f\n",Area);
+printf("Circumference of a circle is:%0.2f",Circumference);
 }
